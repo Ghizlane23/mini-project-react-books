19d164f01c9721f08ff46a98de80ce4a2144c38e
ghizlane-essaidi <ghiza.essaidi@gmail.com>
2023-03-28 03:43:08 +0300

    first mini project without css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BookList from "./components/BookList";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import AddBook from "./components/AddBook";
+import BookDetail from "./components/BookDetail";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/BookList"} element={<BookList />} />
+        <Route path={"/add"} element={<AddBook />} />
+        <Route path={"/detail/:id"} element={<BookDetail />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/BooksData.js
@@ -0,0 +1,20 @@
+const BooksData = [
+  {
+    id: 1,
+    name: "The Alchemist",
+    details:
+      "This international bestseller follows the journey of a shepherd boy named Santiago who travels to Egypt in search of treasure. It's a timeless tale about following your dreams and discovering your true purpose in life. ",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1654371463i/18144590.jpg",
+    available: true,
+  },
+  {
+    id: 2,
+    name: "The Hunger Games",
+    details:
+      "In this dystopian novel, Katniss Everdeen volunteers as tribute for the Hunger Games, a televised competition where teenagers fight to the death. It's a gripping and thought-provoking story about survival, sacrifice, and rebellion.",
+    image:
+      "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41tqzW6kO7S.jpg",
+    available: true,
+  },
+];
